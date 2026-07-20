3332d04126fc10becd8c9587477f0be319847601
Anastasiya-Lemeshko <134445767+Anastasiya-Lemeshko@users.noreply.github.com>
2026-07-20 22:56:16 +0000

    Deploying to gh-pages from @ Anastasiya-Lemeshko/energostrateg@c3ad4b6da1c1849fededcba47eae2986e5101da8 🚀

--- js/main.js
+++ js/main.js
@@ -17295,7 +17295,7 @@ const SLIDER_CONFIG = {
     'desktop_width': SMALL_DESKTOP_WIDTH
   },
   'media': {
-    'mobile_count': 10000,
+    'mobile_count': 1,
     'tablet_count': 3,
     'desktop_count': 4,
     'loop': false,
@@ -18242,18 +18242,18 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   setFilterToggles: () => (/* binding */ setFilterToggles)
 /* harmony export */ });
 const filters = document.querySelector('.filters');
-const filterView = filters ? filters.querySelector('.filters__view') : null;
+const filterView = filters ? filters.querySelector('.controls__view') : null;
 const filterList = filters ? filters.querySelector('.filters__products') : null;
 const setFilterToggles = () => {
   if (!filterView || !filterList) return;
   filterView.addEventListener('click', evt => {
     const button = evt.target.closest('button');
-    filterView.querySelector('.filters__view-toggle--active').classList.remove('filters__view-toggle--active');
-    button.classList.add('filters__view-toggle--active');
+    filterView.querySelector('.controls__view-toggle--active').classList.remove('controls__view-toggle--active');
+    button.classList.add('controls__view-toggle--active');
     filterList.classList.remove('filters__products--animated');
     filterList.classList.add('filters__products--hidden');
     setTimeout(() => {
-      if (button.classList.contains('filters__view-toggle--list')) {
+      if (button.classList.contains('controls__view-toggle--list')) {
         filterList.classList.add('filters__products--list', 'products-view-list');
       } else {
         filterList.classList.remove('filters__products--list', 'products-view-list');
